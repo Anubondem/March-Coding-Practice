b53c31953384056a994165799b606916e1c454d3
Faddal Ibrahim <41176161+faddalibrahim@users.noreply.github.com>
2021-03-02 13:46:31 +0000

    complete challenge1 solution

---
+++ FaddalChallenge1Solution.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class FaddalChallenge1Solution {
+
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+
+        Scanner scan = new Scanner(System.in);
+
+        /* Declare second integer, double, and String variables. */
+        int intg;
+        double dbl;
+        String str;
+
+        /* Read and save an integer, double, and String to your variables. */
+        // Note: If you have trouble reading the entire String, please go back and
+        // review the Tutorial closely.
+        intg = scan.nextInt();
+        dbl = scan.nextDouble();
+        scan.nextLine();
+        str = scan.nextLine();
+
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println(intg + i);
+
+        /* Print the sum of the double variables on a new line. */
+        System.out.println(dbl + d);
+
+        /*
+         * Concatenate and print the String variables on a new line; the 's' variable
+         * above should be printed first.
+         */
+        System.out.println(s + str);
+
+        scan.close();
+    }
+}
